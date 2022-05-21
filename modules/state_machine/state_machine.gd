94272dd07361ffaa3fe3b888ffb740b205d663a1
Denis Levy <denis.levi11@gmail.com>
2022-05-21 16:19:57 -0300

    update: state machine

--- modules/state_machine/state_machine.gd
+++ modules/state_machine/state_machine.gd
@@ -12,23 +12,29 @@ var _next_state
 func _ready():
 	_states_map()
 
-	_current_state = _states[initial_state]
-	if _current_state.has_method("_on_ready") : _current_state._on_ready()
-	pass
+	_set_state(_states[initial_state])
 
-func _physics_process(_delta):
+func _physics_process(_delta) -> void:
 	if _current_state != null:
-			if _current_state.has_method("_update"): _current_state._update(_delta)
-			if _current_state.has_method("_transition"):
-				if _current_state._transition() != null:
-					if _current_state.has_method("_on_exit"): _current_state._on_exit()
-					_current_state = _current_state._transition()
-					if _current_state.has_method("_on_ready") : _current_state._on_ready()
-	pass
+			if _current_state.has_method("_update"): 
+				_current_state._update(_delta)
 
-func _states_map():
+			if _current_state.has_method("_transition"):
+					_next_state = _current_state._transition()
+
+			if _next_state != null and _next_state != _current_state: 
+				_set_state(_current_state._transition())
+
+func _states_map() -> void:
 	if get_children().size() > 0: 
 		for state in get_children():
 			state.context = self
 			_states[state.name] = state
-	pass
+
+func _set_state(newState: State) -> void:
+	if _current_state != null:
+		if _current_state.has_method("_on_exit"): _current_state._on_exit()
+
+	_current_state = newState
+
+	if _current_state.has_method("_on_ready"): _current_state._on_ready()
