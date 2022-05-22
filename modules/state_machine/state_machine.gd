cf43469385dcd9409aca88e412409c3efc8cab2d
Denis Levy <denis.levi11@gmail.com>
2022-05-22 18:47:58 -0300

    ...

--- modules/state_machine/state_machine.gd
+++ modules/state_machine/state_machine.gd
@@ -2,12 +2,13 @@ tool
 extends Node
 class_name StateMachine
 
-onready var _states := {}
+export(String) onready var initial_state
 
-export(String) var initial_state
 onready var target := get_parent()
+
+var _states := {}
 var _current_state: State = null
-var _next_state
+var _next_state: State = null
 
 func _ready():
 	_states_map()
@@ -15,15 +16,15 @@ func _ready():
 	_set_state(_states[initial_state])
 
 func _physics_process(_delta) -> void:
-	if _current_state != null:
-			if _current_state.has_method("_update"): 
-				_current_state._update(_delta)
+	if _current_state:
+		if _current_state.has_method("_update"): 
+			_current_state._update(_delta)
 
-			if _current_state.has_method("_transition"):
-					_next_state = _current_state._transition()
+		if _current_state.has_method("_transition"):
+			_next_state = _current_state._transition()
 
-			if _next_state != null and _next_state != _current_state: 
-				_set_state(_current_state._transition())
+		if _next_state != null and _next_state != _current_state: 
+			_set_state(_current_state._transition())
 
 func _states_map() -> void:
 	if get_children().size() > 0: 
@@ -32,7 +33,7 @@ func _states_map() -> void:
 			_states[state.name] = state
 
 func _set_state(newState: State) -> void:
-	if _current_state != null:
+	if _current_state:
 		if _current_state.has_method("_on_exit"): _current_state._on_exit()
 
 	_current_state = newState
